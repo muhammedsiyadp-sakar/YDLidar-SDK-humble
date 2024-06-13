ce8e2311de6e77e27e9e6d90809e3282c286a7d3
muhammedsiyadp <muhammedsiyadofficial@gmail.com>
2024-06-13 15:18:51 +0530

    second commit

---
+++ cmake/test/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/sr05/YDLidar-SDK-humble/test
+# Build directory: /home/sr05/YDLidar-SDK-humble/cmake/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(lidar_test "lidar_test")
+set_tests_properties(lidar_test PROPERTIES  _BACKTRACE_TRIPLES "/home/sr05/YDLidar-SDK-humble/test/CMakeLists.txt;10;add_test;/home/sr05/YDLidar-SDK-humble/test/CMakeLists.txt;0;")
